b5fa1346e4b257fcc144bab1475440f803b0eeda
Peter Fänge <pellefange@gmail.com>
2024-10-24 17:54:19 +0200

    finished the while loop with the added if statements to provide feedback

---
+++ index.js
@@ -0,0 +1,16 @@
+const target = Math.floor(Math.random() * 10)
+let guess = null
+
+while (guess !== target) {
+    guess = parseFloat(prompt("Guess a number between 1 and 10"));
+     
+    if (guess !== target && guess < target) {
+    console.log("Higher");
+    } 
+    else if (guess !== target && guess > target) {
+    console.log("Lower");
+    }
+    else {
+    console.log(`Congratulations! The correct number was ${target}!`);
+    } 
+} 
